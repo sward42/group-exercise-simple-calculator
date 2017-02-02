5cb0167722ac914b794ded5cef71bd1e311651ae
Lee Hankins <bryanhankins@me.com>
2017-02-01 21:02:18 -0800

    if statemnet in projectCS

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,19 +6,52 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 namespace SimpleCalculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
-        {
+        { 
             int counter = 0;
-            Console.Write("[" + counter + "]");
+                     
+                Console.Write("[" + counter + "]"):
             string userMathProblem = Console.ReadLine();
             Expression userExpression = new Expression();
             userExpression.verifyUserMathProblem(userMathProblem);
+
             Console.WriteLine(userExpression.mathFactorA);
+
             Console.WriteLine(userExpression.mathOperator);
+
+            if(userExpression.mathOperator == "+") // call to method goes here instead of "+"
+            {
+                Console.WriteLine("add method");
+            }
+            else if (userExpression.mathOperator == "-")
+            {
+                Console.WriteLine("subtract method");
+            }
+            else if (userExpression.mathOperator == "*")
+            {
+                Console.WriteLine("multiply method");
+            }
+            else if (userExpression.mathOperator == "/")
+            {
+                Console.WriteLine("divide method");
+            }
+            else if (userExpression.mathOperator == "%")
+            {
+                Console.WriteLine("modulus method");
+            }
+            else
+            {
+                Console.WriteLine("please select a valid operator");
+            }
+
+
             Console.WriteLine(userExpression.mathFactorB);
+
             Console.ReadLine();
+           
         }
+
     }
 }
