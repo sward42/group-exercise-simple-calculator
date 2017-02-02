378f804e3cd15b96dd72b417e8c92fd1f48925bf
jessica kilby <jessicakilby@gmail.com>
2017-02-01 18:04:54 -0600

    unit test and other files setup

---
+++ SimpleCalculator.Tests/ModulusTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class ModulusTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
