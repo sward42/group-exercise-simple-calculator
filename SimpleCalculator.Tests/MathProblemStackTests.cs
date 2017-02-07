61402453125f6be3188c088792f069e05683ed46
sward42 <sarahward42@gmail.com>
2017-02-06 21:26:34 -0600

    complete mathProblemTests

---
+++ SimpleCalculator.Tests/MathProblemStackTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class MathProblemStackTests
+    {
+        [TestMethod]
+        public void CheckCreatingStack()
+        {
+            MathProblemStack mathProblems = new MathProblemStack();
+            Assert.IsNotNull(mathProblems);
+        }
+
+        [TestMethod]
+        public void CheckAddNewProblemMethod()
+        {
+            MathProblemStack mathProblems = new MathProblemStack();
+            mathProblems.addNewMathProblem("2 + 2");
+            
+
+            string expectedResult = "2 + 2";
+            string actualResult = mathProblems.Stack.Peek().ToString();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void CheckReturnLastProblemMethod()
+        {
+            MathProblemStack mathProblems = new MathProblemStack();
+            mathProblems.addNewMathProblem("2 + 2");
+            mathProblems.addNewMathProblem("7 + 9");
+
+            string expectedResult = "7 + 9";
+            string actualResult = mathProblems.getLastMathProblem();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
--- SimpleCalculator/Storage/MathProblemStack.cs
+++ SimpleCalculator/Storage/MathProblemStack.cs
@@ -8,16 +8,28 @@ namespace SimpleCalculator
 {
     public class MathProblemStack
     {
-       Stack<string> MathProblems = new Stack<string>();
+        public Stack<string> Stack { get; set; }
+
+        public MathProblemStack()
+        {
+            this.Stack = new Stack<string>();
+        }
+
+        //public MathProblemStack(Stack<string> stack)
+        //{
+        //    this.Stack = stack;
+        //}
+
+       //Stack<string> MathProblems = new Stack<string>();
 
        public void addNewMathProblem (string newMathProblem)
         {
-            MathProblems.Push(newMathProblem);
+            Stack.Push(newMathProblem);
         }
 
         public string getLastMathProblem ()
         {
-            string returnedMathProblem = MathProblems.Peek().ToString();
+            string returnedMathProblem = Stack.Peek().ToString();
             return returnedMathProblem;
         }
     }
